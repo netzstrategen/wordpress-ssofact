8bdaada99fdcee66f9a1bd6511c79e8a4a81738e
Julià Mestieri <juli@netzstrategen.com>
2018-07-31 14:42:49 +0200

    Fixed shop logout not tigger drupal sso logout actions.

--- src/Plugin.php
+++ src/Plugin.php
@@ -530,6 +530,16 @@ class Plugin {
         restore_previous_locale();
       }
 
+      /**
+       * Filters the log out redirect URL.
+       *
+       * @since 4.2.0
+       *
+       * @param string  $redirect_to           The redirect destination URL.
+       * @param string  $requested_redirect_to The requested redirect destination URL passed as a parameter.
+       * @param WP_User $user                  The WP_User object for the user that's logging out.
+       */
+      $redirect_to = apply_filters( 'logout_redirect', $redirect_to, $requested_redirect_to, $user );
       wp_safe_redirect( $redirect_to );
       exit();
     }

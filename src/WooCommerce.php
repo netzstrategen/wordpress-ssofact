1405807d30e5af084dc34c903c7d9ffe3e9608d1
Daniel Kudwien <daniel@netzstrategen.com>
2018-08-15 15:30:40 +0200

    Added help text to explain what customers should do in case the subscriber mapping fails.

--- src/WooCommerce.php
+++ src/WooCommerce.php
@@ -723,6 +723,11 @@ var nfyFacebookAppId = '637920073225349';
     Server::addDebugMessage();
     if (!isset($response['statuscode']) || $response['statuscode'] !== 200) {
       $message = isset($response['userMessages']) ? implode('<br>', $response['userMessages']) : __('Error while saving the changes.');
+      // Add a note to explain what customers should do in case the mapping
+      // remains to fail.
+      $message .= vsprintf(' Sollte das Problem fortbestehen, <a href="%s" target="_blank">kontaktieren Sie bitte unseren Kundenservice</a>.', [
+        site_url('/service/kontaktformular'),
+      ]);
       wc_add_notice($message, 'error');
     }
     else {

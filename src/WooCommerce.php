4af3660a7c3bf558cf06cab84da8f8fea14a2b65
sun <daniel@netzstrategen.com>
2018-08-17 21:08:24 +0200

    Added help text to explain what customers should do in case the subscriber mapping fails. (#10)

--- src/WooCommerce.php
+++ src/WooCommerce.php
@@ -734,7 +734,11 @@ var nfyFacebookAppId = '637920073225349';
     $response = WooCommerce::validateSubscriberId();
     Server::addDebugMessage();
     if (!isset($response['statuscode']) || $response['statuscode'] !== 200) {
-      $message = isset($response['userMessages']) ? implode('<br>', $response['userMessages']) : __('Error while saving the changes.');
+      // Use a custom error message to explain what customers should do in case
+      // the mapping remains to fail.
+      $message = vsprintf('Ihre Abonummer konnte nicht zugeordnet werden. Sollte das Problem fortbestehen, <a href="%s" target="_blank">kontaktieren Sie bitte unseren Kundenservice</a>.', [
+        site_url('/service/kontaktformular'),
+      ]);
       wc_add_notice($message, 'error');
     }
     else {

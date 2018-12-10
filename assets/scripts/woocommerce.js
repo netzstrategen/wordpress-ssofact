38ac0ed02339be2c72c0e46c121a9156fcd8b177
sun <daniel@netzstrategen.com>
2018-12-10 17:52:52 +0100

    Fixed previous address field values are still submitted when switching salutation. (#47)
    The change to the JavaScript is technically no longer necessary with the fix for the server-side processing of POST data, but is kept for now as it was part of the manual testing.

--- assets/scripts/woocommerce.js
+++ assets/scripts/woocommerce.js
@@ -8,14 +8,21 @@ jQuery(document).ready(function ($) {
   $('.field--salutation select').bind('change.ssofact', function () {
     var prefix = this.id.split('_')[0];
     var isCompany = $(this).val() === 'Firma';
-    $('#' + prefix + '_company_field, #' + prefix + '_company_contact_field')
+    var input;
+    input = $('#' + prefix + '_company_field, #' + prefix + '_company_contact_field')
       .toggle(isCompany)
       .toggleClass('validate-required', isCompany)
       .find('input').prop({ required: isCompany });
-    $('#' + prefix + '_first_name_field, #' + prefix + '_last_name_field')
+    if (!isCompany) {
+      input.val('');
+    }
+    input = $('#' + prefix + '_first_name_field, #' + prefix + '_last_name_field')
       .toggle(!isCompany)
       .toggleClass('validate-required', !isCompany)
       .find('input').prop({ required: !isCompany });
+    if (isCompany) {
+      input.val('');
+    }
   }).trigger('change.ssofact');
 
   /**

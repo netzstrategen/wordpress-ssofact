d404f0141d8812535c9d780fbdeadeaad575a780
Daniel Kudwien <daniel@netzstrategen.com>
2018-12-10 19:18:38 +0100

    Fixed validateUser throws error even if no SSO ID was returned.
    Details about expected validation conditions in https://jira.newsfactory.de/browse/HS-192

--- src/WooCommerce.php
+++ src/WooCommerce.php
@@ -1065,17 +1065,26 @@ var nfyFacebookAppId = '637920073225349';
     if (empty($missing_fields)) {
       $response = Server::validateUser($validate_user_data);
       Server::addDebugMessage();
-      if (isset($response['abono'])) {
+      if (isset($response['abono']) || isset($response['id'])) {
         $show_error = TRUE;
         $subscriber_id = get_user_meta(get_current_user_ID(), 'billing_subscriber_id', TRUE);
         $new_subscriber_data = WC()->session->get('subscriber_data');
         $new_subscriber_id = $new_subscriber_data ? $new_subscriber_data['subscriber_id'] : FALSE;
-        if ($subscriber_id === $response['abono'] || $new_subscriber_id === $response['abono']) {
+        // Do not output an error if the current user has the same subscriber ID
+        // or attempts to associate their account with the returned subscriber ID,
+        // and if no SSO ID was returned, or if the returned SSO ID matches the
+        // one of the current logged-in user.
+        if (!empty($response['abono']) && ($subscriber_id === $response['abono'] || $new_subscriber_id === $response['abono'])
+        && (empty($response['id']) || (isset($userinfo['id']) && $response['id'] === $userinfo['id']))) {
           $show_error = FALSE;
         }
-        elseif (get_user_meta(get_current_user_ID(), 'alfa_dummy_address', TRUE)) {
+        // Do not output an error if a subscriber ID and no SSO ID was returned.
+        elseif (!empty($response['abono']) && empty($response['id'])) {
           $show_error = FALSE;
-          if (!$new_subscriber_data) {
+          // Automatically associate the existing subscription, if a subscriber
+          // ID and no SSO ID was returned, and if the current user has a dummy
+          // address (trial subscription).
+          if (!$new_subscriber_data && get_user_meta(get_current_user_ID(), 'alfa_dummy_address', TRUE)) {
             $subscriber_data = [
               'subscriber_id' => $response['abono'],
             ] + $userinfo;
